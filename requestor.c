db42ecd7719de2d555fef0f61ed5bbd2596aa535
tbarkev <thomas.barkevich@outlook.com>
2025-06-27 16:25:28 -0400

    Updates makefile and requestor.c to match slides

--- requestor.c
+++ requestor.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
+#include <curl/curl.h>
 
+#define OK 0
+#define INIT_ERR 1
+#define REQ_ERR 2
 
-int main() {
-    // Print hello message
-    printf("Hello! The program has successfully ran!\n");
-    return 0;
+#define URL "http://localhost:8000"
+
+int main(void){
+    CURL *curl;
+    CURLcode res;
+
+    curl = curl_easy_init();
+    if (curl){
+        curl_easy_setopt(curl, CURLOPT_URL, URL);
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+        res = curl_easy_perform(curl);
+        if(res != CURLE_OK){
+            return REQ_ERR;
+        }
+        curl_easy_cleanup(curl);
+    } else {
+        return INIT_ERR;
+    }
+    return OK;
 }
-
-
